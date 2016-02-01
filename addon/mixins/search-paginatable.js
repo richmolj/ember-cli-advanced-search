ed516a5b6b60c135983b118c3fd129552f89217c
Lee Richmond <lrichmond1@bloomberg.net>
2016-02-01 17:04:53 -0500

    Add ember-cli-pagination compatibility

--- addon/mixins/search-paginatable.js
+++ addon/mixins/search-paginatable.js
@@ -6,6 +6,15 @@ export default Ember.Mixin.create({
   perPage:          Ember.computed.alias('pagination.perPage'),
   totalResultCount: Ember.computed.alias('pagination.total'),
 
+  // Specifically for ember-cli-pagination compatibility
+  // https://github.com/mharris717/ember-cli-pagination/issues/153
+  paginationContent: Ember.computed(function() {
+    return {
+      page: this.get('currentPage'),
+      totalPages: this.get('totalPages')
+    };
+  }),
+
   totalPages: Ember.computed('totalResultCount', 'perPage', function() {
     return Math.ceil(this.get('totalResultCount') / this.get('perPage'));
   })
